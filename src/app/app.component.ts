fa24e08782f7c53f7b1f6abcb0ce834a476799b4
Apenteng Joseph <apentengjoseph59@gmail.com>
2022-12-01 07:49:51 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'scrambly-app';
+  open: boolean = false;
+  isLight: boolean = false
+
+  constructor(){}
+
+
+  openSide() {
+    this.open = !this.open;
+  }
+  toggleTheme() {
+    this.isLight = !this.isLight
+  }
+
+
+  ngOnInit(): void {
+  }
+
 }
